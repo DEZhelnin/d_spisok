e7607fa8c0f3e99f0109425a533ad387c9748b7a
DEZhelnin <91614275+DEZhelnin@users.noreply.github.com>
2022-04-27 23:11:24 +0300

    Update list2.h

--- list2.h
+++ list2.h
@@ -21,3 +21,4 @@ size_t get_content_length();
 void get_form_data(char*& data);
 void get_param_value(char*& value, const char* param_name, const char* data);
 void str_decode(char*& dec_str, const char* enc_str);
+void add_to_begin(list2& list, int data);
